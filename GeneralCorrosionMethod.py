b311f8fbfc490441037839fda011a6c19ce5ce68
alexandr-izmailov <sashaizmalov@gmail.com>
2023-01-29 22:40:02 +0300

    Добавлена функция f_t_nom(pipe_type,t,M_ut)

--- GeneralCorrosionMethod.py
+++ GeneralCorrosionMethod.py
@@ -1,9 +1,25 @@
-def plus(a,b):
-    return a+b
-def minus(a,b):
-    return a-b
+# input data
+t = 3.7592
+pipe_type = 'Seamless'
+M_ut = 0
+
+
+def f_t_nom(pipe_type, t, M_ut):
+    """"
+    t_nom - nominal or furnished thickness of the component adjusted for mill undertolerance as applicable, [mm]
+    """
+    t_nom = 0.00000
+    text = ''
+    if pipe_type == 'Seamless' or pipe_type == 'User defined %':
+        t_nom = t * (1 - M_ut)
+        text = f't_nom = t * (1 - M_ut) = {t} * (1 - {M_ut}) = {t_nom}'
+    elif pipe_type == 'Welded' or pipe_type == 'User defined mm':
+        t_nom = t - M_ut
+        text = f't_nom = t - M_ut = {t} - {M_ut} = {t_nom}'
+    return t_nom, text
+def t_ml():
+    pass
 
 
 if __name__ == '__main__':
-    print(plus(1,2))
-    print(minus(1,2))
+    print(f_t_nom(pipe_type, t, M_ut))
